2e43d7b6ef510f5f194147d0945c0a4a9cc43a52
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-01-23 15:40:00 +0100

    Testing sum operation

--- SnowCover_studies/IT-Snow/monthSWE.R
+++ SnowCover_studies/IT-Snow/monthSWE.R
@@ -3,32 +3,35 @@
 # to store values in a clean way, without need to compute them every time.
 
 library(ncdf4)
+library(matrixStats)
 
 setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow")
 
-# We have to address a problem that could lead to erraneous results. In fact there is
-# no built-in operator for matrices that is able to treat the NAs as we want to. We will
-# have to define a function that performs operations element-wise and gives NA as a result
-# only when both values are missing. 
-sumNA <- function(a, b){
-  if(is.na(a) & is.na(b)){ return(NA) }
-  sum(a, b, na.rm = TRUE)               # No need to use return (last line always returned)
-}
+mat1 <- matrix(c(NA, NA, 3.1, 2.1), nrow = 2, ncol = 2)
+mat1[is.na(mat1)] <- 0
+mat2 <- matrix(c(1.1, NA, NA, 4.3), nrow = 2, ncol = 2)
+mat2[is.na(mat2)] <- 0
+print("First matrix: ")
+mat1
+print("Second matrix: ")
+mat2
 
+res <- mat1 + mat2
+res
 
-nc <- nc_open("y2011/ITSNOW_SWE_201103.nc")
-time <- ncvar_get(nc, names(nc$var)[1])
-appo <- matrix(0, nrow = nc$dim$Longitude$len, ncol = nc$dim$Latitude$len )
-
-for(i in time){
-  swe_day <- ncvar_get(nc, names(nc$var)[2], start = c(1, 1, i+1), count = c(-1, -1, 1))
-  
-  print(paste0("Working with ", i+1, "/03/2011 SWE map!"))
-  res <- mapply(sumNA, appo, swe_day)
-  appo <- matrix(res, nrow = nrow(appo))
-}
-
-appo <- appo/length(time)
-save(appo, file = "March2011_map.RData")
-
-nc_close(nc)
+# nc <- nc_open("y2011/ITSNOW_SWE_201103.nc")
+# time <- ncvar_get(nc, names(nc$var)[1])
+# appo <- matrix(0, nrow = nc$dim$Longitude$len, ncol = nc$dim$Latitude$len )
+# 
+# for(i in time){
+#   swe_day <- ncvar_get(nc, names(nc$var)[2], start = c(1, 1, i+1), count = c(-1, -1, 1))
+#   swe_day[is.na(swe_day)] <- 0
+#   
+#   print(paste0("Working with ", i+1, "/03/2011 SWE map!"))
+#   appo <- appo + swe_day
+# }
+# 
+# appo <- appo/length(time)
+# save(appo, file = "March2011_map.RData")
+# 
+# nc_close(nc)
