9fb4e8bda37820de054113920dffcf7365d9aa41
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-01-26 16:13:21 +0100

    Prova di plot con cartina geografica sottostante

--- SnowCover_studies/IT-Snow/geoPlot.R
+++ SnowCover_studies/IT-Snow/geoPlot.R
@@ -1,35 +1,44 @@
-# The main goal of this script is to plot SWE distribution, while also having 
-# plotted Italian topography.
-
-library(sf)
-library(ncdf4)
-library(ggplot2)
-library(rnaturalearth)
-
-setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow")
-
-# Opening netCDF file and loading what we want to plot (this being the SWE snapshot
-# of the first day of March). We will not get into details, because we already covered
-# them in the previous couple of R files.
-f_name <- "y2011/ITSNOW_SWE_201103.nc"
-nc <- nc_open(f_name)
-lat_name = names(nc$dim)[1]
-lon_name = names(nc$dim)[2]
-swe_name = names(nc$var)[2]
-lat <- ncvar_get(nc, lat_name)
-lon <- ncvar_get(nc, lon_name)
-swe <- ncvar_get(nc, swe_name, start = c(1, 1, 1), count = c(-1, -1, 1))
-
-
-# Plotting snapshot without Italian boundaries. We covered this topic in the 
-# previous file, so we will skip documenting lines.
-grid <- expand.grid(lon = lon, lat = lat)
-grid$swe <- as.vector(swe)
-ggplot(grid, aes(x = lon, y = lat, fill = grid$swe)) + 
-  geom_raster() + 
-  scale_fill_viridis_c(option = "C") + 
-  coord_fixed() + 
-  labs(title = "SWE 1 March 2011", x = "Latitude", y = "Longitude", fill = "SWE (mm w.e.)") + 
-  theme_minimal()
-
-nc_close(nc)
+  # The main goal of this script is to plot SWE distribution, while also having
+  # plotted Italian topography.
+  rm(list = ls())
+  gc()
+  
+  library(sf)
+  library(ncdf4)
+  library(ggplot2)
+  library(rnaturalearth)
+  
+  setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow")
+  
+  # Opening netCDF file and loading what we want to plot (this being the SWE snapshot
+  # of the first day of March). We will not get into details, because we already covered
+  # them in the previous couple of R files.
+  f_name <- "y2011/ITSNOW_SWE_201103.nc"
+  nc <- nc_open(f_name)
+  lat_name = names(nc$dim)[1]
+  lon_name = names(nc$dim)[2]
+  swe_name = names(nc$var)[2]
+  lat <- ncvar_get(nc, lat_name)
+  lon <- ncvar_get(nc, lon_name)
+  swe <- ncvar_get(nc, swe_name, start = c(1, 1, 1), count = c(-1, -1, 1))
+  
+  
+  # Selecting the geographical background, in order to really understand where the
+  # snow coverage actually is
+  europe <- ne_countries(continent = "Europe", scale = 10, returnclass = "sf")
+  
+  
+  # Plotting snapshot without Italian boundaries. We covered this topic in the
+  # previous file, so we will skip documenting lines.
+  grid <- expand.grid(lon = lon, lat = lat)
+  grid$swe <- as.vector(swe)
+  
+  ggplot() +
+    geom_sf(data = europe, fill = "grey90", color = "black", inherit.aes = FALSE) +
+    coord_sf(xlim = c(6, 19), ylim = c(37, 46.8)) +
+    geom_raster(data = grid, aes(x = lon, y = lat, fill = swe)) +
+    scale_fill_viridis_c(option = "C", na.value = "transparent") +
+    labs(title = "SWE 1 March 2011", x = "Llongitude", y = "Latitude", fill = "SWE (mm w.e.)") +
+    theme_minimal()
+  
+  nc_close(nc)
