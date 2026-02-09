76b680de981956f6b406f4f88ba8e341c0679d59
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-02-09 18:30:25 +0100

    Checking for faulty pixels, probabily an IT-Snow bug!

---
+++ SnowCover_studies/IT-Snow/SCD-from-SWE/finding_faulty.R
@@ -0,0 +1,5 @@
+# The main goal of this script is to find out why I have those faulty pixels. 
+# I mean, why are there in the first place? Which filter is necessary to get rid
+# of them? Those are the questions which need an answer.
+rm(list = ls())
+gc()
