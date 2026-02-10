c43fb39f59024946c238b3e215eb6e3e891cd3d2
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-02-10 15:29:27 +0100

    Small refactoring

---
+++ SnowCover_studies/IT-Snow/SCD-from-SWE/Superfluous/Plot/further_check_Venice.R
@@ -0,0 +1,8 @@
+# We already localized faulty pixels in Comacchio valleys. We should now focus on
+# 2011 and 2013 years, which look suspicious. I would like to make a plot containing
+# four subplots. The first row would be concerning 2011, whilst the second one 2013.
+# The left panel should contain SCD values for the whole region and the other one 
+# should show only pixels identified as faulty.
+rm(list = ls())
+gc()
+
