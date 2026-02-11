0353330e5e5c7cec42b48846dd5060298211b158
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-02-11 11:01:27 +0100

    Made a snow density map

---
+++ SnowCover_studies/IT-Snow/SCD-from-RHO/Superfluous/density_map.R
@@ -0,0 +1,43 @@
+# The main goal of this script is to inspect snow density map properties. This is
+# the first time I deal with sucha variable, so making a plot could be useful.
+rm(list = ls())
+gc()
+
+library(sf)
+library(ncdf4)
+library(ggplot2)
+library(rnaturalearth)
+
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow")
+
+# Opening netCDF file and loading what we want to plot (this being the SWE snapshot
+# of the first day of March). We will not get into details, because we already covered
+# them in the previous couple of R files.
+f_name <- "y2011/ITSNOW_RhoS_201103.nc"
+nc <- nc_open(f_name)
+lat_name = "Latitude"
+lon_name = "Longitude"
+swe_name = "RhoS"
+lat <- ncvar_get(nc, lat_name)
+lon <- ncvar_get(nc, lon_name)
+swe <- ncvar_get(nc, swe_name, start = c(1, 1, 1), count = c(-1, -1, 1))
+
+
+# Selecting the geographical background, in order to really understand where the
+# snow coverage actually is
+europe <- ne_countries(continent = "Europe", scale = 10, returnclass = "sf")
+
+# Plotting snapshot without Italian boundaries. We covered this topic in the
+# previous file, so we will skip documenting lines.
+grid <- expand.grid(lon = lon, lat = lat)
+grid$swe <- as.vector(swe)
+
+ggplot() +
+  geom_sf(data = europe, fill = "grey90", color = "black", inherit.aes = FALSE) +
+  coord_sf(xlim = c(6, 19), ylim = c(37, 46.8)) +
+  geom_raster(data = grid, aes(x = lon, y = lat, fill = swe)) +
+  scale_fill_viridis_c(option = "C", na.value = "transparent") +
+  labs(title = "Snow density 01/03/2011", x = "Longitude", y = "Latitude", fill = "Rho [kg/m^3]") +
+  theme_minimal()
+
+nc_close(nc)
