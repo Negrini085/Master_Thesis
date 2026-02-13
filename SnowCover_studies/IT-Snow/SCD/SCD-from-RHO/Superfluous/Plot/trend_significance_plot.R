43ebf6a7c0e9aaa985df37a34cb5c2361afb0df9
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-02-13 18:02:38 +0100

    Trend significance map

---
+++ SnowCover_studies/IT-Snow/SCD/SCD-from-RHO/Superfluous/Plot/trend_significance_plot.R
@@ -0,0 +1,62 @@
+# The main goal of this script is to plot trend significance
+rm(list = ls())
+gc()
+  
+library(sf)
+library(ncdf4)
+library(terra)
+library(ggplot2)
+library(rnaturalearth)
+  
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow/SCD/SCD-from-RHO")
+
+
+
+# Opening netCDF file of the variable we want to plot and importing dem model that
+# will be used to clip standard deviation map to the actual extent of italian territory
+f_name <- "Datas/pval_map.nc"
+nc <- nc_open(f_name)
+lat_name = "lat"
+lon_name = "lon"
+std_name = "Pval"
+lat <- ncvar_get(nc, lat_name)
+lon <- ncvar_get(nc, lon_name)
+pval <- ncvar_get(nc, std_name)
+nc_close(nc)
+
+dem <- rast("Datas/DEM_Italy.tif")
+dem <- as.matrix(dem, wide = TRUE)
+dem <- dem[nrow(dem):1, ]
+dem <- t(dem)
+
+pval[is.na(pval)] <- 1
+pval[is.na(dem)] <- NA
+
+  
+  
+# Selecting the geographical background, in order to really understand where the
+# snow coverage actually is
+europe <- ne_countries(continent = "Europe", scale = 10, returnclass = "sf")
+  
+# Plotting snapshot without Italian boundaries. We covered this topic in the
+# previous file, so we will skip documenting lines.
+grid <- expand.grid(lon = lon, lat = lat)
+grid$pval <- as.vector(pval)
+
+breaks  <- c(0, 0.05, 0.1, Inf)
+labels  <- c("95% confidence", "90% confidence", "Non significant")
+
+grid$pval_class <- cut(grid$pval, breaks = breaks, labels = labels, right = TRUE)
+cols <- c(
+  "95% confidence"  = "red",
+  "90% confidence" = "orange",
+  "Non significant" = "grey"
+)
+  
+ggplot() +
+  geom_sf(data = europe, fill = "grey90", color = "black", inherit.aes = FALSE) +
+  coord_sf(xlim = c(6, 19), ylim = c(37, 46.8)) +
+  geom_raster(data = grid, aes(x = lon, y = lat, fill = pval_class)) +
+  scale_fill_manual(values = cols, na.value = "transparent", drop = FALSE) +
+  labs(title = "Trend significance map", x = "Longitude", y = "Latitude", fill = "Standard\ndeviation (days)") +
+  theme_minimal()
---
+++ SnowCover_studies/IT-Snow/SCD/SCD-from-RHO/trend_significance_map.R
@@ -0,0 +1,52 @@
+# The main goal of this script is to evaluate trend significance by means of MK test
+rm(list = ls())
+gc()
+
+library(ncdf4)
+library(trend)
+library(future.apply)
+plan(multicore, workers = 8)
+options(future.globals.maxSize = 3500 * 1024^2)
+
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/IT-Snow/SCD/SCD-from-RHO")
+fname <- "Datas/season_maps_SCD.nc"
+
+get_p_value <- function(x){
+  
+  # Quality check on NAs and data variability (maybe not the best for high elevation)
+  if (sum(!is.na(x)) < 3 || sd(x, na.rm = TRUE) == 0) {
+    return(NA)
+  }
+  
+  return(mk.test(x)$p.value)
+}
+
+
+
+# Importing SCD maps to be used for MK testing
+nc <- nc_open(fname)
+lon <- ncvar_get(nc, names(nc$dim)[1])
+lat <- ncvar_get(nc, names(nc$dim)[2])
+scd <- ncvar_get(nc, names(nc$var)[1])
+nc_close(nc)
+
+
+
+# MK test
+mk_map <- future_apply(scd, c(1, 2), get_p_value, future.seed = TRUE)
+rm(scd)
+gc()
+
+
+# Saving average map on netCDF file in order to later plot it/analyze it using tools
+lat_dim <- ncdim_def("lat", "degrees_north", lat)
+lon_dim <- ncdim_def("lon", "degrees_east", lon)
+
+scd_var <- ncvar_def(
+  name = "Pval", units = "Probability", dim = list(lon_dim, lat_dim),
+  missval = NA, longname = "P-value MK test", prec = "float"
+)
+
+nc_out <- nc_create("Datas/pval_map.nc", vars = list(scd_var))
+ncvar_put(nc_out, scd_var, mk_map)
+nc_close(nc_out)
