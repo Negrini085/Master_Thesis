28f6d596b9768135cd3f7c1f21f885a28c3b5f26
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-02-27 18:36:20 +0100

    Made mean swe map+

--- SnowCover_studies/Po-Basin/swe_mean_map.R
+++ SnowCover_studies/Po-Basin/swe_mean_map.R
@@ -7,17 +7,17 @@ setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Po-Basin")
 
 
 years <- 1992:2021
-mean_scd <- numeric(0)
+mean_swe <- numeric(0)
 for(y in years){
-  fname <- paste0("Datas/Seasonal_SCD/SCD_", y, ".tif")
+  fname <- paste0("Datas/seasonal_swe_maps/swe_map_", y, ".tif")
   if(file.exists(fname)){
     
     # Collecting snow cover days for the whole investigated period
-    if(y == 1992) mean_scd <- rast(fname)
-    else mean_scd <- mean_scd + rast(fname)
+    if(y == 1992) mean_swe <- rast(fname)
+    else mean_swe <- mean_swe + rast(fname)
   }
 }
 
 # Actually evaluating mean map
-mean_scd <- mean_scd/length(years)
-writeRaster(mean_scd, "Datas/mean_SCD.tif", overwrite = TRUE)
+mean_swe <- mean_swe/length(years)
+writeRaster(mean_swe, "Datas/swe_mean_map.tif", overwrite = TRUE)
