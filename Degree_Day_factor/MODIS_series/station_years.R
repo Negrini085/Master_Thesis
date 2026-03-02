f266c3e6af51bce5bf72e592748adab949c21b78
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-03-02 18:03:31 +0100

    Starting to work with italian stations

---
+++ Degree_Day_factor/MODIS_series/station_years.R
@@ -0,0 +1,64 @@
+# The main goal of this script is to evaluate when measures started and ended at 
+# a specific station.
+rm(list = ls())
+gc()
+
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/Degree_Day_factor/MODIS_series/")
+
+# Function to evaluate operating years. I will check first row content, as well as 
+# whole year values to check whether aws station was actually active or not.
+read_station <- function(fname) {
+
+  # Opening data-frame and selecting values and years
+  df <- read.table(fname, header = FALSE, sep = "", fill = TRUE)
+  raw <- as.matrix(df[, -(1:2), drop = FALSE])
+  years <- as.numeric(df[[1]])
+  
+  # Selecting operational years
+  years <- table(years)
+  names(years) <- NULL
+  
+  # Evaluating which years contain actual datas. It's necessary because in Swiss
+  # there are some stations whose datas are full of -90.
+  logic_years <- array(TRUE, dim = c(length(years)))
+  for(i in seq_len(years)){
+    hs_series <- unlist(lapply(12, function(j) {
+      x <- raw[j + (i-1)*12, ]
+      x <- x[!is.na(x)]
+      x[x == -90] <- NA
+      x
+    }), use.names = FALSE)
+    
+    # Logical mask (checking if even a single data is here)
+    mask <- !is.na(hs_series)
+    appo <- length(hs_series[mask])
+    
+    if(appo == 0){
+      logic_years[i] <- FALSE
+    }
+  }
+  
+  # Looking for the start of data series. Considering that here we are interested
+  # in whole hydrological years, the actual start is one year grater than the one 
+  # where datas start to appear
+  ind <- 1
+  repeat{
+    if(logic_years[ind] == TRUE){
+      break
+    }
+    ind <- ind + 1
+  }
+  start <- years[ind] + 1
+  
+  # Looking for the end of data series
+  ind <- length(years)
+  repeat{
+    if(logic_years[ind] == TRUE){
+      break
+    }
+    ind <- ind - 1
+  }
+  end <- years[ind]
+
+  return(c(start, end))
+}
