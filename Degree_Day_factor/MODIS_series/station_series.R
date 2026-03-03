c440d31e391cde79f3f37340ee800c921f074106
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-03-03 15:14:57 +0100

    Ready to compute MODIS snow cover time series

--- Degree_Day_factor/MODIS_series/station_series.R
+++ Degree_Day_factor/MODIS_series/station_series.R
@@ -9,7 +9,7 @@ library(terra)
 setwd("/home/filippo/Desktop/Codicini/Master_Thesis/Degree_Day_factor/MODIS_series/")
 
 # Function to extract station snow coverage series from MODIS dataset
-snow_series <- function(station_name, station_lon, station_lat, start_year, end_year){
+snow_series <- function(station_lon, station_lat, start_year, end_year){
 
   sc_series <- numeric(0)
   if(start_year < 2000){ start_year <- 2000}
@@ -22,14 +22,15 @@ snow_series <- function(station_name, station_lon, station_lat, start_year, end_
     for(i in seq_len(ndays)){
       
       # Importing daily map
-      fname <- paste0("../../SnowCover_studies/MODIS/Dataset/daily/", y, "/day_", i, ".tif")
+      fname <- paste0("../../SnowCover_studies/MODIS/Dataset/daily/", y, "/day_", sprintf("%03d", i), ".tif")
+      print(paste0("Dealing with day ", sprintf("%03d", i), " of ", ndays, " for year ", y, "!"))
       daily_map <- rast(fname)
       
       # Selecting pixel
       ind <- cellFromXY(daily_map, cbind(station_lon, station_lat))
-      appo <- daily_map[ind]
+      appo <- as.numeric(daily_map[ind])
       
-      # Elongting series
+      # Elongating series
       if(is.na(appo)){ appo <- -90.0}
       sc_series <- c(sc_series, appo)
       rm(daily_map)
@@ -57,6 +58,32 @@ num_maps <- function(){
 }
 
 
+# Function to compute the start of MODIS series in result matrix
+start_res <- function(start_year){
+  start_pos <- 1
+  
+  # If measurements start before the year 2000, I will fill the column 
+  # right from the start
+  if(start_year <= 2000){ 
+    return(start_pos)
+    }
+  
+  # Here I actually have to compute start position. 312 is the number of MODIS 
+  # daily maps available for year 2000
+  start_pos <- 312
+  
+  if(start_year == 2001){
+    return(start_pos+1)
+  }
+  
+  fill_year <- 2001:(start_year-1)
+  for(y in fill_year){
+    if(y %% 4 == 0){ start_pos <- start_pos + 366}
+    else{ start_pos <- start_pos + 365}
+  }
+  
+  return(start_pos+1)
+}
 
 
 
@@ -75,6 +102,26 @@ ntot <- num_maps()
 nstat <- length(station_names)
 station_series <- array(NA, dim = c(ntot, nstat))
 
+# Cycle over stations
 for(i in seq_len(length(station_names))){
-  appo <-
+  
+  # Evaluating start position and snow cover series
+  print(station_names[i])
+  start_pos <- start_res(start_year[i]) 
+  appo <- snow_series(coord_ele[i, 1], coord_ele[i, 2], start_year[i], end_year[i])
+  
+  # Saving single station series
+  df <- data.frame(days = seq_len(length(appo)), sc_series = appo)
+  write.table(df, paste0("Datas/station_series/", station_names[i]))
+  
+  # Saving into massive container
+  station_series[start_pos:(start_pos + length(appo)-1), i] <- appo
+  rm(appo)
+  gc()
 }
+
+
+# Saving station series
+df_out <- as.data.frame(station_series)
+colnames(df_out) <- station_names
+write.table(df_out, "Datas/station_series_all.dat", row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
