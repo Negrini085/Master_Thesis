2ee1ed9e0361fb52cc9a3958f559b603d2991936
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-03-05 11:48:26 +0100

    Small fix

--- SnowCover_studies/Alpine3D/Historical/inspect_dataset.R
+++ SnowCover_studies/Alpine3D/Historical/inspect_dataset.R
@@ -1,18 +1,31 @@
-# The main goal of this script is to inspect SWE_1962_2023.nc content, in order
-# to start analizing it. I feel like that this could filly 32G RAM.
+# The main goal of this script is to inspect Alpine 3D recent dataset.
 rm(list = ls())
 gc()
 
 library(ncdf4)
 
-fname <- "Dataset/SWE_1962_2023.nc"
-setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Alpine3D/Historical/")
+fname <- "Dataset/2016/OSHD_DATA_2015-09.nc"
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Alpine3D/Recent/")
 
 
 # Importing dataset
 nc <- nc_open(fname)
+swe <- ncvar_get(nc, "swe")
 time <- ncvar_get(nc, "time")
-print(max(time))
+ncatt_get(nc, "swe")
+ncatt_get(nc, "smro")
+nc_close(nc)
+
+
+# Dimension names and stuffs like that
 print(names(nc$var))
 print(names(nc$dim))
-nc_close(nc)
+
+
+# SWE matrix dimensions
+print(paste0("SWE matrix dimensions: ", dim(swe)))
+
+
+# Focusing on coordinates
+print(nc$dim$easting)
+print(nc$dim$northing)
