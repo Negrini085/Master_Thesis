6a03a61766f80f2ac3181b49c28dedbcb1469c0b
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-03-10 19:09:07 +0100

    Starting to analize also long term series

--- SnowCover_studies/Alpine3D/Historical/inspect_dataset.R
+++ SnowCover_studies/Alpine3D/Historical/inspect_dataset.R
@@ -4,28 +4,30 @@ gc()
 
 library(ncdf4)
 
-fname <- "Dataset/2016/OSHD_DATA_2015-09.nc"
-setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Alpine3D/Recent/")
+fname <- "Dataset/SWE_1962_2023.nc"
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Alpine3D/Historical/")
 
 
-# Importing dataset
+# Importing dataset and printing dimension names and stuffs like that
 nc <- nc_open(fname)
-swe <- ncvar_get(nc, "swe")
-time <- ncvar_get(nc, "time")
-ncatt_get(nc, "swe")
-ncatt_get(nc, "smro")
-nc_close(nc)
-
-
-# Dimension names and stuffs like that
 print(names(nc$var))
 print(names(nc$dim))
 
+# How many layers are there?
+time <- ncvar_get()
 
-# SWE matrix dimensions
-print(paste0("SWE matrix dimensions: ", dim(swe)))
 
-
-# Focusing on coordinates
-print(nc$dim$easting)
-print(nc$dim$northing)
+# swe <- ncvar_get(nc, "swe")
+# time <- ncvar_get(nc, "time")
+# ncatt_get(nc, "swe")
+# ncatt_get(nc, "smro")
+# 
+# 
+# # SWE matrix dimensions
+# print(paste0("SWE matrix dimensions: ", dim(swe)))
+# 
+# 
+# # Focusing on coordinates
+# print(nc$dim$easting)
+# print(nc$dim$northing)
+nc_close(nc)
