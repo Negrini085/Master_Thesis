d92a3c75f35c5a329db0c7e6a0ecf9150bffef31
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-08-21 19:55:10 +0200

    Making Po Basin plots thesis ready

--- SC_studies/Po-Basin/Superfluous/Plot/swe_evolution_plot.R
+++ SC_studies/Po-Basin/Superfluous/Plot/swe_evolution_plot.R
@@ -5,7 +5,7 @@ gc()
 
 library(ggplot2)
 
-setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SnowCover_studies/Po-Basin")
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SC_studies/Po-Basin")
 
 
 # We first read the table. In order to have a correct plot, we need to take into
@@ -49,18 +49,86 @@ df <- data.frame(
 )
 
 data0 <- as.Date("1991-10-03")
-dates <- as.Date(paste0(seq(1991, 2021, by = 3), "-10-03"))
+dates <- as.Date(paste0(seq(1992, 2021, by = 4), "-01-01"))
 dates_ind <- as.numeric(dates - data0) + 1
 
 
-# Plotting options (we will do a better job when it's all finished)
-ggplot(df, aes(x = day, y = swe)) + 
+p <- ggplot(df, aes(x = day, y = swe)) +
+  geom_line(
+    linewidth = 1.2,
+    colour = "#1F4E79",
+    lineend = "round"
+  ) +
   scale_x_continuous(
     breaks = dates_ind,
-    labels = format(dates, "%d %b %Y"), 
-    expand = c(5e-3, 5e-3)
+    labels = format(dates, "%Y"),
+    expand = expansion(mult = c(0.005, 0.005))
   ) +
-  geom_line(color = "blue", size = 1.5) +
-  labs(title = "SWE evolution on Po Basin: 1992 to 2021", x = "Days", y = "SWE Gm^3") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  scale_y_continuous(
+    expand = expansion(mult = c(0, 0.04))
+  ) +
+  labs(
+    x = "Date",
+    y = expression(bold("SWE [Gm"^3*"]"))
+  ) +
+  theme_classic(base_size = 12) +
+  theme(
+    text = element_text(
+      family = "sans",
+      colour = "black"
+    ),
+    axis.title = element_text(
+      size = 22,
+      face = "bold"
+    ),
+    axis.title.x = element_text(
+      margin = margin(t = 15)
+    ),
+    axis.title.y = element_text(
+      margin = margin(r = 15)
+    ),
+    axis.text = element_text(
+      size = 18,
+      colour = "black"
+    ),
+    axis.text.x = element_text(
+      angle = 45,
+      hjust = 1,
+      vjust = 1
+    ),
+    axis.line = element_line(
+      linewidth = 0.6,
+      colour = "black"
+    ),
+    axis.ticks = element_line(
+      linewidth = 0.5,
+      colour = "black"
+    ),
+    axis.ticks.length = unit(0.15, "cm"),
+    panel.grid.major.x = element_line(
+      colour = "grey80",
+      linewidth = 0.35
+    ),
+    panel.grid.major.y = element_line(
+      colour = "grey80",
+      linewidth = 0.35
+    ),
+    panel.grid.minor = element_blank(),
+    plot.margin = margin(
+      t = 5,
+      r = 5,
+      b = 5,
+      l = 5
+    )
+  )
+
+print(p)
+
+ggsave(
+ "Images/swe_evolution.png",
+ plot = p,
+ width = 14,
+ height = 6,
+ units = "in",
+ dpi = 600
+)
