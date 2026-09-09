00f7571ad74f649c003ea3d58a733de60bc0964a
Negrini085 <filippo.negrini@studenti.unimi.it>
2026-09-09 10:59:42 +0200

    Checking time coverage, everything is all good

---
+++ SWE_model/QC_datas/Input/TEMP/check_time_coverage.R
@@ -0,0 +1,26 @@
+# The main goal of this script is to check weather some daily maps are missing or
+# something like that.
+rm(list = ls())
+gc()
+
+setwd("/home/filippo/Desktop/Codicini/Master_Thesis/SWE_model/QC_datas/Input/TEMP/")
+years <- 1951:2023
+
+for(y in years){
+  fname <- paste0("../../../Input/TEMP/temperatures_", y, ".nc")
+  if(!file.exists(fname)) stop(paste0("No temperature file for ", y))
+  
+  # Opening precipitation file
+  nc <- nc_open(fname)
+  tmax <- ncvar_get(nc, "tmxd")
+  
+  # Selecting year length
+  len <- 365
+  if(y %% 4 == 0) len <- 366
+  
+  # Checking length
+  if(len != dim(tmax)[3]) stop(paste0("No correct amount of days for", y))
+  rm(tmax)
+  rm(nc)
+  gc()
+}
